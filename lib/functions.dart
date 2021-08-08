fa51a0bf38a5bab7badc4c3c7303f091ab92911f
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-08-08 22:39:41 +0000

    Cleaned the code a little further

--- lib/functions.dart
+++ lib/functions.dart
@@ -4,20 +4,20 @@ class Functions {
   int _questionNumber = 0;
   List<Question> _questions = [
     Question(
-        q: "The CPU is responsible for executing instructions for the Computer",
-        a: true),
+        question: "The CPU is responsible for executing instructions for the Computer",
+        answer: true),
     Question(
-        q: "Main memory is a place where the programs and data are stored temporarily during processing",
-        a: true),
+        question: "Main memory is a place where the programs and data are stored temporarily during processing",
+        answer: true),
     Question(
-        q: "Pseudo-code uses exact programming language syntax to represent a module in the larger program",
-        a: false),
+        question: "Pseudo-code uses exact programming language syntax to represent a module in the larger program",
+        answer: false),
     Question(
-        q: "Secondary storage,similar to main memory,also stores programs and data",
-        a: true),
+        question: "Secondary storage,similar to main memory,also stores programs and data",
+        answer: true),
     Question(
-        q: "Writing Code is the first step in the waterfall model of developing software",
-        a: false)
+        question: "Writing Code is the first step in the waterfall model of developing software",
+        answer: false)
   ];
   void nextQuestion() {
     if (_questionNumber < _questions.length - 1) {
--- lib/questions.dart
+++ lib/questions.dart
@@ -2,8 +2,5 @@ class Question {
   late String question;
   late bool answer;
 
-  Question({required String q, required bool a}) {
-    question = q;
-    answer = a;
-  }
+  Question({required this.question, required this.answer});
 }
